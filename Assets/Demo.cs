486f96c54ca6f8f7ba55f43787e783b4d6d4608e
TEAM-LAB\komikado <komikado@team-lab.com>
2017-08-30 22:21:16 +0900

    init

---
+++ Assets/Demo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using komietty.Math;
+
+public class Demo : MonoBehaviour {
+
+    public int nDistribution = 10000;
+    public int nInitialize = 100;
+    public int nlimit = 100;
+    public GameObject pointprefab;
+    private Vector3[] data;
+    private MCMC mcmc;
+
+	void Start () {
+        data = new Vector3[nDistribution];
+        Prepare();
+        mcmc = new MCMC(data);
+        StartCoroutine(Generate());
+    }
+
+    void Prepare()
+    {
+        for(int i=0; i< nDistribution; i++)
+        {
+            var center = (Random.value > 0.5f) ? Vector3.one : Vector3.zero;
+            Vector3 pos = 2 * GaussianDistributionCubic.GenerateRandomPointStandard();
+            pos.x += 5 * RandomGenerator.rand_lnormal(0f, 1f);
+            pos.y += 5 * RandomGenerator.rand_lnormal(0f, 1f);
+            pos.z += 5 * RandomGenerator.rand_lnormal(0f, 1f);
+            data[i] = pos;
+        }
+    }
+
+    IEnumerator Generate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.001f);
+            foreach (var pos in mcmc.Sequence(nInitialize, nlimit))
+            {
+                //yield return new WaitForSeconds(0.001f);
+                Instantiate(pointprefab, pos, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/MCMC.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace komietty.Math
+{
+    public class MCMC
+    {
+        public const int LIMIT_RESET_LOOP_COUNT = 100;
+        public Vector3[] Data { get; private set; }
+
+        private Vector3 _curr;
+        private float _currDensity = 0f;
+
+        public MCMC(Vector3[] data)
+        {
+            this.Data = data;
+        }
+
+        public void Reset()
+        {
+            for (var i = 0; _currDensity <= 0f && i < LIMIT_RESET_LOOP_COUNT; i++)
+            {
+                _curr = new Vector3(Random.value, Random.value, Random.value);
+                _currDensity = Density(_curr);
+            }
+        }
+
+        public IEnumerable<Vector3> Sequence(int nInitialize, int limit)
+        {
+            return Sequence(nInitialize, limit, 0);
+        }
+        public IEnumerable<Vector3> Sequence(int nInitialize, int limit, int nSkip)
+        {
+            Reset();
+
+            for (var i = 0; i < nInitialize; i++)
+                Next();
+
+            for (var i = 0; i < limit; i++)
+            {
+                for (var j = 0; j < nSkip; j++)
+                    Next();
+                yield return _curr;
+                Next();
+            }
+        }
+
+        void Next()
+        {
+            Vector3 next = GaussianDistributionCubic.GenerateRandomPointStandard() + _curr;
+
+            var densityNext = Density(next);
+            if (_currDensity <= 0f || Mathf.Min(1f, densityNext / _currDensity) >= Random.value)
+            {
+                _curr = next;
+                _currDensity = densityNext;
+            }
+        }
+
+        float Density(Vector3 pos)
+        {
+            float weight = 0f;
+            for (int i = 0; i < Data.Length; i++)
+            {
+                float mag = Vector3.SqrMagnitude(pos - Data[i]);
+                weight += Mathf.Exp(-mag);
+            }
+            return weight;
+        }
+    }
+}
